3e04775d789e9baf67ea1daa691be51349b4ee38
eudesfish <114193384+eudesfish@users.noreply.github.com>
2024-08-30 12:51:25 -0300

    Update settings.py

--- gerenciamento_assinaturas/settings.py
+++ gerenciamento_assinaturas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-au1@m6r=8!e3r8)wzg_*k9ey36+*^uq=agtm0_2kn72y1*x0-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eudesnobre.pythonanywhere.com']
 
 
 # Application definition
